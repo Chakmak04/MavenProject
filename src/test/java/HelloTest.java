18815a7a925011afde4890c50cdcae0f2c96de90
My name <chakmak04@gmail.com>
2023-01-24 20:09:34 +0100

    work 1

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,11 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("HelloTest");
+
+        //work 1
+
+        //work 2
+
+        //work 3
     }
 }
